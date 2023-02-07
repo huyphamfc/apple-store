23feaec9c372d674b19aa0be7cc15c48be3113d2
huyphamfc <huyphamfc@gmail.com>
2023-02-07 11:16:27 +0700

    access Cart state

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import styles from './Header.module.scss';
+import { RootState } from 'store';
 
 function Header() {
+  const { totalAmount } = useSelector((state: RootState) => state.cart);
+
   return (
     <header className={styles.header}>
       <div className={styles.header__content}>
         <h1>Apple Store</h1>
         <p>
-          Your Cart <span className={styles.header__badge}>999</span>
+          Your Cart <span className={styles.header__badge}>{totalAmount}</span>
         </p>
       </div>
     </header>
